bb65a88b39b237f10b77f3cb4edd2e48f9aaa406
noman2002 <nomn2002@gmail.com>
2021-07-26 01:52:36 +0530

    initial commits

---
+++ lib/screens/announcement_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+ class AnnouncementPage extends StatelessWidget {
+   const AnnouncementPage({ Key? key }) : super(key: key);
+ 
+   @override
+   Widget build(BuildContext context) {
+     return Container(
+       
+     );
+   }
+ }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,42 +1,62 @@
+
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:technojam_app/screens/announcement_page.dart';
+import 'package:technojam_app/screens/setting_page.dart';
+import 'package:technojam_app/screens/team_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int selectedIndex = 0;
+  final List<Widget> screen = [HomePage(), AnnouncementPage(), TeamPage(), SettingPage()];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xFF233C4D),
-      appBar: AppBar(
-        title: Text("TechnoJam"),
+    return MaterialApp(
+      home: Scaffold(
         backgroundColor: Color(0xFF233C4D),
+        appBar: AppBar(
+          title: Text("TechnoJam"),
+          backgroundColor: Color(0xFF233C4D),
+        ),
+        bottomNavigationBar: CurvedNavigationBar(
+          index: selectedIndex,
+          height: 50.0,
+          buttonBackgroundColor: Colors.pink,
+          // color: Color(0xFF82adc9),
+          backgroundColor: Color(0xFF233C4D),
+          items: [
+            Icon(
+              Icons.home,
+              size: 30,
+            ),
+            Icon(
+              Icons.campaign,
+              size: 30,
+            ),
+            Icon(
+              Icons.people,
+              size: 30,
+            ),
+            Icon(
+              Icons.tune,
+              size: 30,
+            ),
+          ],
+          onTap: (index) {
+            setState(() {
+              selectedIndex = index;
+              print(selectedIndex);
+            });
+          },
+        ),
+        body: null,
       ),
-      bottomNavigationBar: CurvedNavigationBar(
-        height: 50.0,
-        buttonBackgroundColor: Colors.pink,
-        // color: Color(0xFF82adc9),
-        backgroundColor: Color(0xFF233C4D),
-        items: [
-          Icon(
-            Icons.home,
-            size: 30,
-          ),
-          Icon(
-            Icons.campaign,
-            size: 30,
-          ),
-          Icon(
-            Icons.people,
-            size: 30,
-          ),
-          Icon(
-            Icons.tune,
-            size: 30,
-          ),
-        ],
-      ),
-      body: null,
     );
   }
 }
