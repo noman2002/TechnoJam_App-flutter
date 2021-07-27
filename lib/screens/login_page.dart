41083828f60bcfc21af9555ed55302cec1299734
noman2002 <nomn2002@gmail.com>
2021-07-27 23:32:36 +0530

    login screen

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -8,32 +8,108 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String _email = "", _password = "";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF233C4D),
-      body: Column(
-        // mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(60.0),
-            child: Center(
-              child: Image.asset(
-                "assets/images/tjlogo.png",
-                height: 200,
-                width: 200,
+      body: SingleChildScrollView(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 70.0),
+              child: Center(
+                child: Image.asset(
+                  "assets/images/tjlogo.png",
+                  height: 150,
+                ),
               ),
             ),
-          ),
-          Container(
-            height: 300,
-            width: 300,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(50),
+            Container(
+              margin: EdgeInsets.only(top: 40),
+              width: 300,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(50),
+              ),
+              child: Form(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        "LOGIN",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.mail),
+                          hintText: "Enter Email",
+                          labelText: "Email",
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "username cannot be empty";
+                          }
+                          return null;
+                        },
+                        // onChanged: (value) {
+                        //   name = value;
+                        //   setState(() {});
+                        // },
+                        onSaved: (input) {
+                          _email = input!;
+                        },
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.lock),
+                          hintText: "Enter Password",
+                          labelText: "Password",
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Password cannot be empty";
+                          } else if (value.length < 6) {
+                            return "Password length should be at least 6";
+                          }
+                          return null;
+                        },
+                        onSaved: (input) {
+                          _password = input!;
+                        },
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      FloatingActionButton(
+                        backgroundColor: Colors.pink,
+                        child: Icon(Icons.arrow_forward),
+                        onPressed: null,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
