7b495dbfcf1bc3a139e693da5e5be4cbaead733f
noman2002 <nomn2002@gmail.com>
2021-07-27 22:55:18 +0530

    login screen

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -10,8 +10,31 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: null,
+    return Scaffold(
+      backgroundColor: Color(0xFF233C4D),
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(60.0),
+            child: Center(
+              child: Image.asset(
+                "assets/images/tjlogo.png",
+                height: 200,
+                width: 200,
+              ),
+            ),
+          ),
+          Container(
+            height: 300,
+            width: 300,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(50),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
