7bcd3fa2087e3fe454b2f41dea0ca7970e8cabac
noman2002 <nomn2002@gmail.com>
2021-08-05 01:53:46 +0530

    announcement screen

--- lib/screens/announcement_page.dart
+++ lib/screens/announcement_page.dart
@@ -11,7 +11,76 @@ class AnnouncementPage extends StatelessWidget {
         title: Text("Announcements"),
         backgroundColor: Color(0xFF233C4D),
       ),
-      body: null,
+      body: AnnouncementCard(
+        imageUrl: "https://avatars.githubusercontent.com/u/54404474?v=4",
+        title: "Nhi bataunga",
+        desc: "jaaaaduuuuu",
+      ),
     );
   }
 }
+
+class AnnouncementCard extends StatelessWidget {
+  const AnnouncementCard({
+    Key? key,
+    required this.imageUrl,
+    required this.title,
+    required this.desc,
+  }) : super(key: key);
+  final String imageUrl, title, desc;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: 300,
+        color: Colors.grey,
+        child: Container(
+          width: double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(6),
+              child: Image.network(
+                imageUrl,
+                fit: BoxFit.scaleDown,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+// ListTile(
+//         leading: Container(
+//           width: double.infinity,
+//           height: double.infinity,
+//           child: ClipRRect(
+//             borderRadius: BorderRadius.circular(6),
+//             child: Image.network(
+//               imageUrl,
+//               fit: BoxFit.cover,
+//             ),
+//           ),
+//         ),
+//         title: Padding(
+//           padding: const EdgeInsets.all(8.0),
+//           child: Text(
+//             title,
+//             style: TextStyle(
+//               fontSize: 16,
+//               fontWeight: FontWeight.bold,
+//             ),
+//           ),
+//         ),
+//         subtitle: Text(
+//           desc,
+//           style: TextStyle(
+//             fontSize: 16,
+//             fontWeight: FontWeight.bold,
+//           ),
+//         ),
+//       ),
