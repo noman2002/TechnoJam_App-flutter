ead387b577ef9b339c1cd915f4bff97595e97677
noman2002 <nomn2002@gmail.com>
2021-08-04 23:04:01 +0530

    announcement screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:technojam_app/screens/announcement_page.dart';
 import 'package:technojam_app/screens/login_page.dart';
 import 'package:technojam_app/screens/start_page.dart';
 
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(),
-      home: StartPage(),
+      home: AnnouncementPage(),
     );
   }
 }
--- lib/screens/announcement_page.dart
+++ lib/screens/announcement_page.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
- class AnnouncementPage extends StatelessWidget {
-   const AnnouncementPage({ Key? key }) : super(key: key);
- 
-   @override
-   Widget build(BuildContext context) {
-     return Container(
-       
-     );
-   }
- }
 
+class AnnouncementPage extends StatelessWidget {
+  const AnnouncementPage({Key? key}) : super(key: key);
 
- 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF233C4D),
+    );
+  }
+}
