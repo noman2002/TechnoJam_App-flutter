bebc4b9c450478a45a3623b3b30cdb99945c2546
noman2002 <nomn2002@gmail.com>
2021-08-20 23:24:58 +0530

    team page commits

--- lib/screens/team_page.dart
+++ lib/screens/team_page.dart
@@ -1,8 +1,34 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:technojam_app/components/team_card.dart';
 
-class TeamPage extends StatelessWidget {
+class TeamPage extends StatefulWidget {
   const TeamPage({Key? key}) : super(key: key);
+
+  @override
+  _TeamPageState createState() => _TeamPageState();
+}
+
+class _TeamPageState extends State<TeamPage> {
+  List _items = [];
+
+  Future<void> readJson() async {
+    final String response =
+        await rootBundle.loadString('assets/json/members.json');
+    final data = await json.decode(response);
+    setState(() {
+      _items = data["members"];
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    this.readJson();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,62 +41,73 @@ class TeamPage extends StatelessWidget {
       body: Container(
         margin: EdgeInsets.symmetric(vertical: 16),
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              TeamCard(
-                imageUrl:
-                    "https://avatars.githubusercontent.com/u/54404474?v=4",
-                name: "Noman",
-                desc: "Flutter",
-                linkedUrl: "https://www.linkedin.com/in/noman-khan-3b9407183/",
-                githubUrl: "https://github.com/noman2002/",
-              ),
-              TeamCard(
-                imageUrl:
-                    "https://avatars.githubusercontent.com/u/54404474?v=4",
-                name: "Noman",
-                desc: "Flutter",
-              ),
-              TeamCard(
-                imageUrl:
-                    "https://avatars.githubusercontent.com/u/54404474?v=4",
-                name: "Noman",
-                desc: "Flutter",
-              ),
-              TeamCard(
-                imageUrl:
-                    "https://avatars.githubusercontent.com/u/54404474?v=4",
-                name: "Noman",
-                desc: "Flutter",
-              ),
-              TeamCard(
-                imageUrl:
-                    "https://avatars.githubusercontent.com/u/54404474?v=4",
-                name: "Noman",
-                desc: "Flutter",
-              ),
-              TeamCard(
-                imageUrl:
-                    "https://avatars.githubusercontent.com/u/54404474?v=4",
-                name: "Noman",
-                desc: "Flutter",
-              ),
-              TeamCard(
-                imageUrl:
-                    "https://avatars.githubusercontent.com/u/54404474?v=4",
-                name: "Noman",
-                desc: "Flutter",
-              ),
-              TeamCard(
-                imageUrl:
-                    "https://avatars.githubusercontent.com/u/54404474?v=4",
-                name: "Noman",
-                desc: "Flutter",
-              ),
-            ],
+          child: ListView.builder(
+            itemBuilder: (context, index) {
+              return TeamCard(
+                imageUrl: _items[index]["imageUrl"],
+                name: _items[index]["name"],
+                desc: _items[index]["desc"],
+              );
+            },
           ),
         ),
       ),
     );
   }
 }
+
+
+// Column(
+//             children: [
+//               TeamCard(
+//                 imageUrl:
+//                     "https://avatars.githubusercontent.com/u/54404474?v=4",
+//                 name: "Noman",
+//                 desc: "Flutter",
+//                 linkedUrl: "https://www.linkedin.com/in/noman-khan-3b9407183/",
+//                 githubUrl: "https://github.com/noman2002/",
+//               ),
+//               TeamCard(
+//                 imageUrl:
+//                     "https://avatars.githubusercontent.com/u/54404474?v=4",
+//                 name: "Noman",
+//                 desc: "Flutter",
+//               ),
+//               TeamCard(
+//                 imageUrl:
+//                     "https://avatars.githubusercontent.com/u/54404474?v=4",
+//                 name: "Noman",
+//                 desc: "Flutter",
+//               ),
+//               TeamCard(
+//                 imageUrl:
+//                     "https://avatars.githubusercontent.com/u/54404474?v=4",
+//                 name: "Noman",
+//                 desc: "Flutter",
+//               ),
+//               TeamCard(
+//                 imageUrl:
+//                     "https://avatars.githubusercontent.com/u/54404474?v=4",
+//                 name: "Noman",
+//                 desc: "Flutter",
+//               ),
+//               TeamCard(
+//                 imageUrl:
+//                     "https://avatars.githubusercontent.com/u/54404474?v=4",
+//                 name: "Noman",
+//                 desc: "Flutter",
+//               ),
+//               TeamCard(
+//                 imageUrl:
+//                     "https://avatars.githubusercontent.com/u/54404474?v=4",
+//                 name: "Noman",
+//                 desc: "Flutter",
+//               ),
+//               TeamCard(
+//                 imageUrl:
+//                     "https://avatars.githubusercontent.com/u/54404474?v=4",
+//                 name: "Noman",
+//                 desc: "Flutter",
+//               ),
+//             ],
+//           ),
