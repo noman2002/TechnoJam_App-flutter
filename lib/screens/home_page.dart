ade80db80b82246128e05f866d8ef5fe2a292b76
noman2002 <nomn2002@gmail.com>
2021-08-22 23:16:55 +0530

    home page commits

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -6,13 +6,15 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
+
+
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF233C4D),
       appBar: AppBar(
-        
+
         title: Text("TechnoJam"),
         backgroundColor: Color(0xFF233C4D),
       ),
