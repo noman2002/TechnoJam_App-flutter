6f716fef2278b6de276609ac8d5070123688e549
noman2002 <nomn2002@gmail.com>
2021-08-23 22:32:10 +0530

    landing page commits

--- lib/screens/landing_page.dart
+++ lib/screens/landing_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:technojam_app/screens/login_page.dart';
 
 class LandingPage extends StatelessWidget {
   const LandingPage({Key? key}) : super(key: key);
@@ -8,54 +9,120 @@ class LandingPage extends StatelessWidget {
     return Material(
       child: Container(
         color: Color(0xFF233C4D),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              "assets/images/tjlogo.png",
-              fit: BoxFit.contain,
-            ),
-            SizedBox(
-              height: 100,
-            ),
-            Text(
-              "TechnoJam",
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Image.asset(
+                "assets/images/tjlogo.png",
+                fit: BoxFit.contain,
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Throttle",
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.blue[300],
+              Text(
+                "TechnoJam",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Throttle",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.blue[300],
+                    ),
+                  ),
+                  Text(
+                    "To",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    "Learn",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.pink,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 80,
+              ),
+              Text(
+                "Member of Technojam",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ConstrainedBox(
+                constraints: BoxConstraints(
+                  minWidth: 300,
+                  minHeight: 40,
+                ),
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.pink),
+                  ),
+                  onPressed: () {
+                     Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => LoginPage(),
+                        ),
+                      );
+                  },
+                  child: Text(
+                    "Login",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                   ),
                 ),
-                Text(
-                  "To",
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.white,
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                "Not a member. No worries !!",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              ConstrainedBox(
+                constraints: BoxConstraints(
+                  minWidth: 300,
+                  minHeight: 40,
+                ),
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.pink),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    "Use as Guest",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                   ),
                 ),
-                Text(
-                  "Learn",
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.pink,
-                  ),
-                ),
-              ],
-            ),
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
