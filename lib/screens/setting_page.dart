a42db3454cd3b22d8831c031c1b2770b1f2bd90b
noman2002 <nomn2002@gmail.com>
2021-08-24 00:32:28 +0530

    settings page commits

--- lib/screens/setting_page.dart
+++ lib/screens/setting_page.dart
@@ -16,23 +16,78 @@ class _SettingPageState extends State<SettingPage> {
     return Scaffold(
         backgroundColor: Color(0xFF233C4D),
         appBar: AppBar(
-          title: Text("Settings"),
+          title: Text("Extras"),
           backgroundColor: Color(0xFF233C4D),
         ),
         body: Column(
           children: [
             ListTile(
               leading: Icon(
-                Icons.settings,
-                // color: Colors.black,
+                Icons.school_outlined,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Alumni",
+                textScaleFactor: 1.3,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.emoji_events_outlined,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Achievers",
+                textScaleFactor: 1.3,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.settings_suggest_outlined,
+                color: Colors.white,
               ),
               title: Text(
                 "Settings",
                 textScaleFactor: 1.3,
                 style: TextStyle(
-                  color: Colors.black,
+                  color: Colors.white,
                 ),
               ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.lightbulb_outline,
+                color: Colors.white,
+              ),
+              title: Text(
+                "About Us",
+                textScaleFactor: 1.3,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.power_settings_new,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Sign Out",
+                textScaleFactor: 1.3,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              onTap: () {
+                _auth.signOut();
+              },
             ),
           ],
         ));
